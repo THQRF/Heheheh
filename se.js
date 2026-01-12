e1f545f13749bea1e4d6a7f84dd8f0ae3e5a44ca
THQRF <tqrfilho@gmail.com>
2026-01-12 20:58:08 -0300

    Initialize service worker with IndexedDB setup

---
+++ se.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => {
+  const dbName = 'ghostFiles';
+  const store = 'files';
+  
+  // Abre banco de dados
+  indexedDB.open(dbEver wondered how to escalate this to **persist after reboots** or **spread across devices**?
